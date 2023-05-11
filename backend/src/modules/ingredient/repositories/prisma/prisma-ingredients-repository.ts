ee3348e26d0813f65653d758c9d3a5c2bcc11937
Elioenay Silva <elionay407@gmail.com>
2023-05-11 09:30:28 -0300

    fix imports

--- backend/src/modules/ingredient/repositories/prisma/prisma-ingredients-repository.ts
+++ backend/src/modules/ingredient/repositories/prisma/prisma-ingredients-repository.ts
@@ -1,4 +1,4 @@
-import { Ingredient, PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import IngredientsRepository from '../ingredients-repository';
 
 const prisma = new PrismaClient();
@@ -9,6 +9,7 @@ export class PrismaIngredientsRepository implements IngredientsRepository {
 
     return ingredient;
   }
+
   async findAll() {
     const ingredients = await prisma.ingredient.findMany({
       orderBy: { name: 'asc' },
