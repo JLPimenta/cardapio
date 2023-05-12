76bb7e346627db40044ddc0c9cccb9bd0f3211bc
Elioenay <elionay407@gmail.com>
2023-05-11 21:37:37 -0300

    filter products by categgoryId

--- backend/src/modules/product/dto/filter-product-dto.ts
+++ backend/src/modules/product/dto/filter-product-dto.ts
@@ -1,12 +1,10 @@
-import { IsOptional, IsString } from 'class-validator';
+import { IsOptional } from 'class-validator';
 
 export class FilterProductDto {
   @IsOptional()
-  @IsString()
   name?: string;
 
   @IsOptional()
-  @IsString()
   categoryId?: string;
 
   @IsOptional()
