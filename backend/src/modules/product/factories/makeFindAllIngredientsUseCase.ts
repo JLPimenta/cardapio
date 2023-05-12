5ee0a2e6480f3ec64fb83f343976d03d1432795f
Elioenay <elionay407@gmail.com>
2023-05-12 00:20:00 -0300

    find all ingredients of a product

--- backend/src/modules/product/factories/makeFindAllIngredientsUseCase.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaProductsRepository } from '../repositories/prisma/prisma-products-repository';
-import findAllIngredients from '../use-cases/find-all-ingredients';
-
-export function makeFindAllIngredientsUseCase() {
-  const productRepository = new PrismaProductsRepository();
-
-  return new findAllIngredients(productRepository);
-}
--- backend/src/modules/product/product.controller.ts
+++ backend/src/modules/product/product.controller.ts
@@ -6,20 +6,15 @@ import {
   Patch,
   Post,
   Query,
-  ServiceUnavailableException,
 } from '@nestjs/common';
 
 import { CreateProductDto } from './dto/create-product-dto';
 import { makeCreateProductUseCase } from './factories/makeCreateProductUseCase';
-import { Prisma, PrismaClient, Product } from '@prisma/client';
+import { Product } from '@prisma/client';
 
 import { makeFindAllProductsUseCase } from './factories/makeFindAllProductsUseCase';
 import { makeChangeProductAvailabilityUseCase } from './factories/makeChangeProductAvailabilityUseCase';
 import { makeFindOneByIdUseCase } from './factories/makeFindOneByIdUseCase';
-import { makeFindAllIngredientsUseCase } from './factories/makeFindAllIngredientsUseCase';
-import { ingredientsOnProducts } from './repositories/products-repository';
-
-const prisma = new PrismaClient();
 
 @Controller('products')
 export class ProductController {
@@ -54,12 +49,5 @@ export class ProductController {
     const findOneByIdUseCase = makeFindOneByIdUseCase();
 
     return await findOneByIdUseCase.execute(id);
-  }
-
-  @Get('ingredients/:productId')
-  async findAllIngredients(@Param('productId') id: string) {
-    const findAllIngredientsUseCase = makeFindAllIngredientsUseCase();
-
-    return await findAllIngredientsUseCase.execute(id);
   }
 }
