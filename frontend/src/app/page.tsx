c69d472c52509b6df164aa75baddd8f2e6e8fc99
Elioenay <elionay407@gmail.com>
2023-05-25 01:31:51 -0300

    #frontend - update button

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-
         flexDirection: 'column',
         gap: 24,
       }}
@@ -18,6 +17,7 @@ export default function Home() {
           alignItems: 'center',
           justifyContent: 'space-between',
           width: 327,
+
           marginTop: 24,
           marginLeft: 24,
           marginRight: 24,
@@ -50,6 +50,30 @@ export default function Home() {
           01
         </div>
       </header>
+
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+
+          position: 'fixed',
+          bottom: 36,
+        }}
+      >
+        <button
+          style={{
+            width: 327,
+            height: 50,
+
+            backgroundColor: '#FB923C',
+            borderRadius: 6,
+            color: '#fff',
+          }}
+        >
+          itens no pedido
+        </button>
+      </div>
 
       <div
         style={{
@@ -139,6 +163,8 @@ export default function Home() {
           flexDirection: 'column',
           gap: 12,
           width: 327,
+
+          paddingBottom: 90,
         }}
       >
         <div
@@ -153,6 +179,50 @@ export default function Home() {
             borderRadius: 6,
 
             padding: 12,
+            paddingBottom: 12,
+          }}
+        >
+          <div style={{ display: 'flex', flexDirection: 'column', gap: 8 }}>
+            <span style={{ fontSize: 14, fontWeight: 'bold' }}>X-Tudo</span>
+            <span style={{ fontSize: 12 }}>
+              Pão, hambúrguer caseiro, queijo mussarela, bacon, Catupiry,
+              salsicha, presunto, ovo, milho, batata palha, alface, tomate e
+              nossa deliciosa maionese caseira.
+            </span>
+            <span style={{ fontSize: 14, fontWeight: 'bold' }}>R$ 15,00</span>
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+
+              borderRadius: 6,
+            }}
+          >
+            <Image
+              src='/x-tudo.png'
+              width={400}
+              height={65}
+              alt='x-tudo'
+              quality={100}
+              priority
+            />
+          </div>
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+
+            gap: 8,
+
+            border: '1px solid #dddedf',
+            borderRadius: 6,
+
+            padding: 12,
+            paddingBottom: 12,
           }}
         >
           <div style={{ display: 'flex', flexDirection: 'column', gap: 8 }}>
