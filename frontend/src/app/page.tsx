9bba1274c91e5c85034fa1f862c561bc21947a06
Elioenay <elionay407@gmail.com>
2023-05-31 23:26:21 -0300

    change text color

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -60,8 +60,8 @@ export default function Home() {
       </div>
 
       <div className="flex h-[6.375rem] w-[21.875rem] flex-col items-center justify-center gap-1 rounded-lg bg-orange-100">
-        <span className="text-sm">Total dos meus pedidos</span>
-        <span className="text-3xl font-bold">R$ 25,00</span>
+        <span className="text-sm text-gray-900">Total dos meus pedidos</span>
+        <span className="text-3xl font-bold text-gray-900">R$ 25,00</span>
       </div>
 
       {loading ? (
